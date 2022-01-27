a6bd4cc10f7f5fc6b7d994844f8d0a3df890ea1c
Jordan D. Nelson <Jordan.doug.nelson@gmail.com>
2022-01-27 10:55:42 -0600

    table is loaded

---
+++ components/DataTableWrapper.vue
@@ -0,0 +1,42 @@
+<!-- Please remove this file from your project -->
+<template>
+<DataTable
+  :header-fields="headerFields"
+  :data="data">
+</DataTable>
+</template>
+
+<script>
+import { DataTable } from 'v-datatable-light';
+
+export default {
+  name: "DataTableWrapper",
+  components: {
+    DataTable
+  },
+  data: function() {
+    return {
+      data: [
+        {
+          ticker: 'GOOG',
+          price: 12.3
+        },
+        {
+          ticker: 'AAPL',
+          price: 30.3
+        }
+      ],
+      headerFields: [
+        {
+          name: 'ticker',
+          label: 'Ticker',
+        },
+        {
+          name: 'price',
+          label: 'Price'
+        }
+      ]
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Welcome to Skopos AI</h1>
-    <DataTable />
+    <DataTableWrapper />
   </div>
 </template>
 
@@ -9,11 +9,9 @@
 export default {
   name: "IndexPage",
   async asyncData({ $content, params }) {
-    const data = await $content('price_data_short').limit(500).fetch()
-    const result = {
-      data: data
-    }
-    return result;
+    const data = await $content('price_data_short').fetch();
+    console.log(data);
+    return {};
   }
 };
 </script>
