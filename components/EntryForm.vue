8bb329cc43e7f7c933a24e4f6d32b891996ded68
Jordan D. Nelson <Jordan.doug.nelson@gmail.com>
2022-01-27 13:35:19 -0600

    ability to add tickers to list

--- components/EntryForm.vue
+++ components/EntryForm.vue
@@ -10,6 +10,16 @@
           :select-size="4"
         ></b-form-select>
       </b-form-group>
+      <b-form-group id="input-group-2" label="Insert a Ticker" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.entry"
+          placeholder="Enter ticker symbol"
+        ></b-form-input> 
+      </b-form-group>
+      <b-form-group>
+        <b-button v-on:click="onAdd()" variant="secondary">Save Ticker</b-button>
+      </b-form-group>
       <b-button type="submit" variant="primary">Submit</b-button>
     </b-form>
   </div>
@@ -20,7 +30,8 @@ export default {
   data() {
     return {
       form: {
-        tickers: []
+        tickers: [],
+        entry: ''
       },
       options: this.tickers
     }
@@ -34,6 +45,10 @@ export default {
   methods: {
     onSubmit (event) { 
       event.preventDefault();
+    },
+    onAdd() {
+      this.tickers.push(this.form.entry);
+      this.form.entry = '';
     }
   }
 }
